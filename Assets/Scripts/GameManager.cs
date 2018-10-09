cfe2a775ed45d64a6ed4a9495a22a35057517974
Hui Qi <e0007393@u.nus.edu>
2018-10-09 11:04:21 +0800

    Fix input

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,37 +7,40 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Z))
+        if (!UIManagerScript._gameOverCheck)
         {
-            playArea.TriggerGate(PlayAreaInputScript.NumPad.ONE);
-        }
-        else if (Input.GetKeyDown(KeyCode.X))
-        {
-            playArea.TriggerGate(PlayAreaInputScript.NumPad.TWO);
-        }
-        else if (Input.GetKeyDown(KeyCode.C))
-        {
-            playArea.TriggerGate(PlayAreaInputScript.NumPad.THREE);
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            playArea.TriggerGate(PlayAreaInputScript.NumPad.FOUR);
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            playArea.TriggerGate(PlayAreaInputScript.NumPad.SIX);
-        }
-        else if (Input.GetKeyDown(KeyCode.Q))
-        {
-            playArea.TriggerGate(PlayAreaInputScript.NumPad.SEVEN);
-        }
-        else if (Input.GetKeyDown(KeyCode.W))
-        {
-            playArea.TriggerGate(PlayAreaInputScript.NumPad.EIGHT);
-        }
-        else if (Input.GetKeyDown(KeyCode.E))
-        {
-            playArea.TriggerGate(PlayAreaInputScript.NumPad.NINE);
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                playArea.TriggerGate(PlayAreaInputScript.NumPad.ONE);
+            }
+            else if (Input.GetKeyDown(KeyCode.X))
+            {
+                playArea.TriggerGate(PlayAreaInputScript.NumPad.TWO);
+            }
+            else if (Input.GetKeyDown(KeyCode.C))
+            {
+                playArea.TriggerGate(PlayAreaInputScript.NumPad.THREE);
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                playArea.TriggerGate(PlayAreaInputScript.NumPad.FOUR);
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                playArea.TriggerGate(PlayAreaInputScript.NumPad.SIX);
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                playArea.TriggerGate(PlayAreaInputScript.NumPad.SEVEN);
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                playArea.TriggerGate(PlayAreaInputScript.NumPad.EIGHT);
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                playArea.TriggerGate(PlayAreaInputScript.NumPad.NINE);
+            }
         }
     }
 }
