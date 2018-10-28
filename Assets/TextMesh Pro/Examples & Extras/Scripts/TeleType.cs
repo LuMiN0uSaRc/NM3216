7a8e5e103b83d9f67f5a6dd287526f45b54c290c
Hui Qi <e0007393@u.nus.edu>
2018-10-28 14:23:32 +0800

    Set up text revealer for speech

--- Assets/TextMesh Pro/Examples & Extras/Scripts/TeleType.cs
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/TeleType.cs
@@ -16,31 +16,14 @@ namespace TMPro.Examples
         private string label02 = "Example <sprite=2> of using <sprite=7> <#ffa000>Graphics Inline</color> <sprite=5> with Text in <font=\"Bangers SDF\" material=\"Bangers SDF - Drop Shadow\">TextMesh<#40a0ff>Pro</color></font><sprite=0> and Unity<sprite=2>";
 
 
-        private TMP_Text m_textMeshPro;
+        private TextMeshProUGUI m_textMeshPro;
 
 
         void Awake()
         {
             // Get Reference to TextMeshPro Component
-            m_textMeshPro = GetComponent<TMP_Text>();
-            m_textMeshPro.text = label01;
+            m_textMeshPro = GetComponent<TextMeshProUGUI>();
             m_textMeshPro.enableWordWrapping = true;
-            m_textMeshPro.alignment = TextAlignmentOptions.Top;
-
-
-
-            //if (GetComponentInParent(typeof(Canvas)) as Canvas == null)
-            //{
-            //    GameObject canvas = new GameObject("Canvas", typeof(Canvas));
-            //    gameObject.transform.SetParent(canvas.transform);
-            //    canvas.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
-
-            //    // Set RectTransform Size
-            //    gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(500, 300);
-            //    m_textMeshPro.fontSize = 48;
-            //}
-
-
         }
 
 
@@ -50,30 +33,30 @@ namespace TMPro.Examples
             // Force and update of the mesh to get valid information.
             m_textMeshPro.ForceMeshUpdate();
 
-
             int totalVisibleCharacters = m_textMeshPro.textInfo.characterCount; // Get # of Visible Character in text object
             int counter = 0;
             int visibleCount = 0;
 
-            while (true)
+            while (visibleCount < totalVisibleCharacters)
             {
                 visibleCount = counter % (totalVisibleCharacters + 1);
 
                 m_textMeshPro.maxVisibleCharacters = visibleCount; // How many characters should TextMeshPro display?
 
-                // Once the last character has been revealed, wait 1.0 second and start over.
-                if (visibleCount >= totalVisibleCharacters)
-                {
-                    yield return new WaitForSeconds(1.0f);
-                    m_textMeshPro.text = label02;
-                    yield return new WaitForSeconds(1.0f);
-                    m_textMeshPro.text = label01;
-                    yield return new WaitForSeconds(1.0f);
-                }
+                //// Once the last character has been revealed, wait 1.0 second and start over.
+                //if (visibleCount >= totalVisibleCharacters)
+                //{
+                //    yield return new WaitForSeconds(1.0f);
+                //    m_textMeshPro.text = label02;
+                //    yield return new WaitForSeconds(1.0f);
+                //    m_textMeshPro.text = label01;
+                //    yield return new WaitForSeconds(1.0f);
+                //}
 
                 counter += 1;
 
-                yield return new WaitForSeconds(0.05f);
+                yield return new WaitForSecondsRealtime(0.02f);
+                //yield return null;
             }
 
             //Debug.Log("Done revealing the text.");
